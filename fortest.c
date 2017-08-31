d99cbbc8588992965f06c2ff871b5e76943dd69d
Rosiweta <mrrk2@naver.com>
2017-08-31 23:39:57 +0900

    first test commit

---
+++ fortest.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+int main()
+{
+	int user_choice;
+
+	while(1)
+	{
+		printf("book managament program \n");
+	
+		printf("select the menu \n");
+
+		printf("1.add book \n");
+
+		printf("2.search book \n");
+
+		printf("3.borrow book \n");
+
+		printf("4.lend book \n");
+
+		printf("5.program shut down \n");
+
+		printf("what is your choice : ");
+		scanf("%d", &user_choice);
+
+		if(user_choice  == 1)
+		{}
+		else if(user_choice == 2)
+		{}
+		else if(user_choice == 3)
+		{}
+		else if(user_choice == 4)
+		{}
+		else if(user_choice == 5)
+		{break;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+	return 0;
+
+}
